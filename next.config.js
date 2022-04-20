a237700c75ea43e2db28312262aec727ed97bc35
Gretchen Schadegg <gschadegg@gmail.com>
2022-04-20 16:46:27 -0700

    removes sentry in project to have on vercel

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-const { withSentryConfig } = require('@sentry/nextjs')
-
-const moduleExports = {
+const nextConfig = {
   reactStrictMode: true,
 }
 
-const sentryWebpackPluginOptions = {
-  silent: true, // Suppresses all logs
-}
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+module.exports = nextConfig
