481321f9c645327d188da8353cf95db4e1df7f15
Gretchen Schadegg <gschadegg@gmail.com>
2022-04-27 14:43:48 -0700

    adds basic quicklink list of tests needed

---
+++ __tests__/unit/quickLinks.unit.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, fireEvent, within } from '@testing-library/react'
+
+describe('Quick Links', () => {
+  it.todo('no quick links loads add button only')
+
+  it.todo('add button does not show after 5 quick links')
+
+  it.todo('loads favicon from url')
+
+  it.todo('shows edit button on hover')
+})
+
+describe('Quick Links Edit Form', () => {
+  it.todo('edit button opens edit form')
+
+  it.todo('edit form updates quick link')
+})
